b10bdde31f2a272c3500f30647046b9789c58712
itland.vs <itland.vs@gmail.com>
2021-11-21 19:22:09 +0300

    Test

--- First/Program.cs
+++ First/Program.cs
@@ -11,7 +11,7 @@ namespace First
         static void Main(string[] args)
         {
             Console.WriteLine("Hello git");
-            Console.WriteLine("gyg");
+            Console.WriteLine("TESTTTTTTTTTT????????!!!!!!!!?!?!??!?!@");
             Console.ReadKey();
         }
     }
